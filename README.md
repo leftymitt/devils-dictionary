39db2f13ff36e5627035e3be0b578d7842c8af6b
orange <treefrog@riseup.net>
2017-05-01 23:43:22 +0000

    tweak for readability (tabs to spaces)

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # the devil's dictionary
 
-an html to yaml converter for ambrose bierce's *the devil's dictionary*. 
-run: 
+an html to yaml converter for ambrose bierce's *the devil's dictionary*. run: 
 
 ```
 $ ./parse_dictionary.sh > devils.dictionary.yml
 ```
 
-the parser doesn't parse everything correctly. 
-some manual post-processing is required. 
+the parser doesn't parse everything correctly. some manual post-processing is
+required. 
--- parse_dictionary.sh
+++ parse_dictionary.sh
@@ -38,20 +38,20 @@ MODE="NEW"
 ################################################################################
 
 function PRINT_WORD() {
-	echo "    - word: ${TERM}"
-	if ! [ -z "${TYPE}" ]; then
-		echo "      type: ${TYPE}"
-	fi
-	if ! [ -z "${DEF}" ]; then
-		echo "      def: >- " 
-		echo "         ${DEF}"
-	fi
-	if ! [ -z "${POEM}" ]; then
-		echo "      poem: |+ "
-		echo "${POEM}"
-#		echo "      author: ${AUTHOR}"
-	fi
-	echo ""
+  echo "    - word: ${TERM}"
+  if ! [ -z "${TYPE}" ]; then
+    echo "      type: ${TYPE}"
+  fi
+  if ! [ -z "${DEF}" ]; then
+    echo "      def: >- " 
+    echo "         ${DEF}"
+  fi
+  if ! [ -z "${POEM}" ]; then
+    echo "      poem: |+ "
+    echo "${POEM}"
+#    echo "      author: ${AUTHOR}"
+  fi
+  echo ""
 }
 
 
@@ -62,68 +62,68 @@ function PRINT_WORD() {
 echo "dictionary:"
 
 while IFS= read -r CMD; do
-	NEWLETTER=$(echo "${CMD}" | sed -e 's/\ //g' | tr -d "\n")
-	if [[ "${NEWLETTER}" =~ ^(A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z)$ ]]; then
-		if ! [ -z "${TERM}" ]; then
-			PRINT_WORD
-		fi
+  NEWLETTER=$(echo "${CMD}" | sed -e 's/\ //g' | tr -d "\n")
+  if [[ "${NEWLETTER}" =~ ^(A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z)$ ]]; then
+    if ! [ -z "${TERM}" ]; then
+      PRINT_WORD
+    fi
 
-		LETTER=${NEWLETTER}
-		echo " - letter: ${LETTER}"
-		echo "   terms:"
+    LETTER=${NEWLETTER}
+    echo " - letter: ${LETTER}"
+    echo "   terms:"
 
-		BLOCK=""
-		NBLOCK=""
+    BLOCK=""
+    NBLOCK=""
 
-		TERM=""
-		TYPE=""
-		DEF=""
-		POEM=""
-		AUTHOR=""
+    TERM=""
+    TYPE=""
+    DEF=""
+    POEM=""
+    AUTHOR=""
 
-		MODE="NEW"
-		continue
-	fi
+    MODE="NEW"
+    continue
+  fi
 
-	CMD=$(echo "${CMD}" | sed -e 's/               /\n/' | sed -e 's/^[ \t]*//g' | sed -e 's/_\([^_]*\)_/\<i\>\1\<\/i\>/g' )
+  CMD=$(echo "${CMD}" | sed -e 's/               /\n/' | sed -e 's/^[ \t]*//g' | sed -e 's/_\([^_]*\)_/\<i\>\1\<\/i\>/g' )
 
-	if [ "${MODE}" == "NEW" ]; then
-		BLOCK=${BLOCK}${CMD}
-	elif [ "${MODE}" == "WAIT" ]; then
-		NBLOCK=${NBLOCK}$(echo "${CMD}" | sed -e 's/^/         /g')$'\n'
-	fi
+  if [ "${MODE}" == "NEW" ]; then
+    BLOCK=${BLOCK}${CMD}
+  elif [ "${MODE}" == "WAIT" ]; then
+    NBLOCK=${NBLOCK}$(echo "${CMD}" | sed -e 's/^/         /g')$'\n'
+  fi
 
-	NEWTERM=$(echo "${CMD}" | sed -n "s/\(^[A-Z][A-Z()\-]*[A-Z]\).*/\1/p")
+  NEWTERM=$(echo "${CMD}" | sed -n "s/\(^[A-Z][A-Z()\-]*[A-Z]\).*/\1/p")
 
-	if ! [ -z "${NEWTERM}" ]; then 
-		if ! [ -z "${TERM}" ]; then 
-			MODE="NEW"
-			PRINT_WORD
+  if ! [ -z "${NEWTERM}" ]; then 
+    if ! [ -z "${TERM}" ]; then 
+      MODE="NEW"
+      PRINT_WORD
 
-			BLOCK="${CMD}"
-			NBLOCK=""
+      BLOCK="${CMD}"
+      NBLOCK=""
 
-			TERM=""
-			TYPE=""
-			DEF=""
-			POEM=""
-			AUTHOR=""
-		fi
-	fi
+      TERM=""
+      TYPE=""
+      DEF=""
+      POEM=""
+      AUTHOR=""
+    fi
+  fi
 
-	if [ "${CMD}" == '' ]; then
-		if [ "${MODE}" == "NEW" ]; then
-			TERM=$(echo "${BLOCK}" | perl -pe "s/(^[A-Z][A-Z| ()or'-]*[A-Z])[.|,] *([\w\.]*)? *(.*)?/\1/p")
-			TYPE=$(echo "${BLOCK}" | perl -pe "s/(^[A-Z][A-Z| ()or'-]*[A-Z])[.|,] *([\w\.]*)? *(.*)?/\2/p")
-			DEF=$(echo "${BLOCK}" | perl -pe "s/(^[A-Z][A-Z| ()or'-]*[A-Z])[.|,] *([\w\.]*)? *(.*)?/\3/p")
+  if [ "${CMD}" == '' ]; then
+    if [ "${MODE}" == "NEW" ]; then
+      TERM=$(echo "${BLOCK}" | perl -pe "s/(^[A-Z][A-Z| ()or'-]*[A-Z])[.|,] *([\w\.]*)? *(.*)?/\1/p")
+      TYPE=$(echo "${BLOCK}" | perl -pe "s/(^[A-Z][A-Z| ()or'-]*[A-Z])[.|,] *([\w\.]*)? *(.*)?/\2/p")
+      DEF=$(echo "${BLOCK}" | perl -pe "s/(^[A-Z][A-Z| ()or'-]*[A-Z])[.|,] *([\w\.]*)? *(.*)?/\3/p")
 
-			if ! [ -z "${TERM}" ]; then
-				MODE="WAIT"
-			fi
-		elif [ "${MODE}" == "WAIT" ]; then
-			POEM=$(echo "${NBLOCK}")
-#			AUTHOR=$(echo "${NBLOCK}" | sed -e '$ d' | sed -e '$ d' | sed -n 's/\ *\([^\s\\]\)/\1/p' | tail -1)
-		fi
-	fi
+      if ! [ -z "${TERM}" ]; then
+        MODE="WAIT"
+      fi
+    elif [ "${MODE}" == "WAIT" ]; then
+      POEM=$(echo "${NBLOCK}")
+#      AUTHOR=$(echo "${NBLOCK}" | sed -e '$ d' | sed -e '$ d' | sed -n 's/\ *\([^\s\\]\)/\1/p' | tail -1)
+    fi
+  fi
 
 done < "$FILE"
